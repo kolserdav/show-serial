483e8385ecacb0f6306e868992afa3425b3422ed
kolserdav <serega12101983@gmail.com>
2022-03-15 13:00:01 +0700

    Added ejs template

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// @ts-check
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -6,6 +8,26 @@ const { SERIAL_PATH } = process.env;
 
 const app = express();
 
-app.use('/serial', express.static(SERIAL_PATH))
+app.set('view engine', 'ejs');
 
-app.listen(3000);
+app.use('/public', express.static('public'));
+
+app.get('/favicon.ico', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'home' });
+});
+
+app.get('/:id', function (req, res) {
+  const { params } = req;
+  const { id } = params;
+  let seriesNum = parseInt(id, 10);
+  seriesNum = Number.isNaN(seriesNum) ? 1 : seriesNum;
+  const prev = seriesNum - 1;
+  const next = seriesNum + 1;
+  res.render('index', { id, prev, next, title: 'player' });
+});
+
+app.use('/serial', express.static(SERIAL_PATH));
+
+app.listen(80);
